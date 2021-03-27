ea47d0dc76bedd01da6c9d4071562fca3cfcc1d1
Khrystyna7388 <khristina7388@gmail.com>
2021-03-27 15:46:27 +0200

    remove and revert buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+  const [arr, changeArr] = useState([
+    {
+      id: 1,
+      tittle: 'card1'
+    },
+    {
+      id: 1,
+      tittle: 'card2'
+    },
+    {
+      id: 1,
+      tittle: 'card3'
+    }
+  ])
+  const [itemsToHide, setItemsToHide] = useState([]);
+
+  const filteredArray = arr.filter(el => !itemsToHide.includes(el.id));
+
+  const removeItems = () => {
+    const itemToRemove = filteredArray[0];
+    setItemsToHide(...itemsToHide, itemToRemove.id);
+  }
+
+  const onRevert = () => {
+    setItemsToHide([]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={removeItems}>remove</button>
+      <button onClick={onRevert}>revert</button>
+      <ul>
+        {filteredArray.map(el => <li key={el.id}>{el.tittle}</li>)}
+      </ul>
     </div>
   );
 }
