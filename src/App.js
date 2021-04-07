8dfaf9c94ad73ed5f03f8614b93ccd59c546edef
Khrystyna7388 <khristina7388@gmail.com>
2021-04-07 16:52:27 +0300

    controlled select

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URL = 'https://jsonplaceholder.typicode.com';
+
+const App = () => {
+    const [endpoint, setEndpoints] = useState('posts');
+
+    const [item, setItem] = useState(null);
+
+    const fetchData = async () => {
+        const response = await fetch(`${URL}/${endpoint}/1`)
+        const data = await response.json();
+        setItem(data);
+    }
+
+    const onSubmit = (e) => {
+        fetchData();
+        alert(JSON.stringify(item, null, 2));
+        e.preventDefault();
+    }
+
+    const onChange = (e) => {
+        setEndpoints(e.target.value)
+    }
+
+    return (
+        <div className="App">
+            <form onSubmit={onSubmit}>
+                <label>
+                    <select onChange={onChange}>
+                        <option value={endpoint}>post</option>
+                        <option value="comments">comment</option>
+                        <option value="albums">album</option>
+                        <option value="photos">photo</option>
+                        <option value="todos">todoo</option>
+                        <option value="users">user</option>
+                    </select>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+
+        </div>
+    );
 }
 
 export default App;
