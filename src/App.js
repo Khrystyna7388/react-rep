9ad0b5fb0d8a7c8ac6adf753c56b9b5f54708367
Khrystyna7388 <khristina7388@gmail.com>
2021-04-08 18:14:55 +0300

    Context 1 part lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
+import React, {useState, createContext, useContext} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CounterContext = createContext();
+
+const ContextProvider = ({children}) => {
+    const [counter, setCounter] = useState(0);
+
+    const incCounter = () => {
+        setCounter(counter + 1);
+    }
+
+    const decCounter = () => {
+        setCounter(counter - 1);
+    }
+
+    return (
+        <CounterContext.Provider value={{
+            counter,
+            incCounter,
+            decCounter
+        }}>
+            {children}
+        </CounterContext.Provider>
+    )
+}
+
+const Counter = () => {
+    const {counter, incCounter, decCounter} = useContext(CounterContext);
+
+    return (
+        <>
+            <h2>Counter: {counter}</h2>
+            <button onClick={incCounter}>inc</button>
+            <button onClick={decCounter}>dec</button>
+        </>
+    )
+}
+
+const Header = () => {
+    const {counter} = useContext(CounterContext);
+
+    return (
+        <h1>Header counter: {counter}</h1>
+    )
+}
+
+const App = () => {
+    return (
+        <ContextProvider>
+            <Header/>
+            <Counter/>
+        </ContextProvider>
+
+    );
 }
 
 export default App;
