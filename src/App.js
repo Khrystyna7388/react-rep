43b15db621e6ad8a88d1ee57335507a3c05e3dfe
Khrystyna7388 <khristina7388@gmail.com>
2021-04-09 00:15:01 +0300

    life cycle_function component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let interval;
+const Comp = () => {
+
+    // useEffect(() => {
+    //   console.log('did mount child')
+    // }, [])
+
+    useEffect(() => {
+        interval = setInterval(() => {
+            console.log('fetching data')
+        }, 2000)
+        return () => clearInterval(interval)
+    }, [])
+
+    return (
+        <h1>child</h1>
+    )
+}
+
+const App = () => {
+    const [counter, setCounter] = useState(0);
+
+    const incCounter = () => {
+        setCounter(counter + 1);
+    }
+
+    useEffect(() => {
+        console.log('did mount parent')
+    }, [])
+
+    useEffect(() => {
+        console.log('did update parent')
+
+        return () => console.log('will unmount parent')
+    }, [counter])
+
+    return (
+        <div className="App">
+            <h1 onClick={incCounter}>hello {counter}</h1>
+            {!!(counter % 2) && <Comp/>}
+        </div>
+    );
 }
 
 export default App;
