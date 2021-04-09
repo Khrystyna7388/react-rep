4d55c1257bd627997a958042d4632b85101a6c7d
Khrystyna7388 <khristina7388@gmail.com>
2021-04-09 12:01:22 +0300

    life cycle_example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+const URL = 'https://jsonplaceholder.typicode.com/todos';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counter, setCounter] = useState(1);
+    const [todo, setTodo] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const incCounter = () => {
+        setCounter(counter + 1);
+    }
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        const response = await fetch(`${URL}/${counter}`);
+        const data = await response.json();
+
+        // setTimeout(() => {
+            setTodo(data);
+            setIsLoading(false);
+        // }, 1000)
+    }
+
+    useEffect(() => {
+        fetchData()
+        return () => {
+            setTodo(null);
+        }
+    }, [counter])
+
+    return (
+        <div className="App">
+            <h1 onClick={incCounter}>hello</h1>
+
+            {isLoading &&
+            (<h1>loading...</h1>
+            )}
+
+            {!!todo &&
+            <>
+                <hr/>
+                <h3>{todo.title} -- {todo.completed.toString()} -- {todo.id}</h3>
+                <hr/>
+            </>
+            }
+        </div>
+    );
 }
 
 export default App;
