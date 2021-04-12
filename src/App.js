6779770d4087b3e5dd1224473941ff0049030bdc
Khrystyna7388 <khristina7388@gmail.com>
2021-04-12 14:16:24 +0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
+import React, {useEffect, useMemo} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//useMemo
+//запам'ятовується результат функції(не відбувається ререндер)
+
+const fn = (a, b) => {
+    console.log('called')
+    return Math.pow(a, b);
+}
+
+const App = () => {
+    const [counter, setCounter] = React.useState(0);
+    const [counter1, setCounter1] = React.useState(2);
+
+    // const complexLogic = fn(4,2);
+    //
+    // const complexLogic = useMemo(() => {
+    //     return fn(4, 2)
+    // }, [])
+    // console.log('rerender', complexLogic)
+
+    const complexLogic1 = useMemo(() => {
+        return fn(4, counter1)
+    }, [counter1])
+
+    console.log('logic1', complexLogic1)
+
+    return (
+        <div className="App">
+            <button onClick={() => setCounter(prev => prev + 1)}>{counter}</button>
+            <button onClick={() => setCounter1(prev => prev + 1)}>{counter1}</button>
+        </div>
+    );
 }
 
 export default App;
