1eecf5e39b59f25b9fe22fab98021e6022af31bb
Khrystyna7388 <khristina7388@gmail.com>
2021-04-12 12:45:01 +0300

    useState callback style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
 
-function App() {
+// useState callback style
+// якщо нове значення стейту похідне від попереднього значення(щоб позбутись асинхронності)
+
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const inc = () => {
+    // setCounter(counter + 1);
+    setCounter(prev => prev + 1)
+  }
+
+  // useEffect(() => {
+  //   console.log(counter)
+  // }, [counter])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={inc}>{counter}</button>
     </div>
   );
 }
